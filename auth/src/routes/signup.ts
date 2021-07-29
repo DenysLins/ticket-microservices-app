09d957d3dfaeac0be92ce911206c6168e409db8c
Denys André dos Santos Lins <denyslins@gmail.com>
2021-07-28 21:32:40 -0300

    Update signup and add validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,27 @@
 import express from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send({ status: 200 });
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 6, max: 20 })
+      .withMessage("Password must be between 6 and 20 characters"),
+  ],
+  (req: express.Request, res: express.Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    res.send({ status: 200, email, password });
+  }
+);
 
 export { router as signUpRouter };
