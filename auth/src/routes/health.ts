8d2dcd19194c2ac2fe4ccdb3df50a1bc4a6b401a
Denys André dos Santos Lins <denyslins@gmail.com>
2021-08-07 13:44:08 -0300

    Add protected health

--- auth/src/routes/health.ts
+++ auth/src/routes/health.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import { currentUser } from "../middlewares/current-user";
+import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 
 router.get("/api/users/health", (req, res) => {
   res.send({ status: "ok" });
 });
+
+router.get(
+  "/api/users/health-protected",
+  currentUser,
+  requireAuth,
+  (req, res) => {
+    res.send({ status: "ok" });
+  }
+);
 
 export { router as healthRouter };
