cb5cbc8e31985577a123a1b95d27791882c20b20
Denys André dos Santos Lins <denyslins@gmail.com>
2021-08-06 21:24:07 -0300

    Update current-user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
+import { verifyJwt } from "./../services/jwt";
 import express from "express";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  res.send({ status: 200 });
-});
+router.get(
+  "/api/users/currentuser",
+  (req: express.Request, res: express.Response) => {
+    const token = req.session?.jwt;
+
+    try {
+      const payload = verifyJwt(token);
+      return res.send({ currentUser: payload });
+    } catch (error) {
+      return res.send({ currentUser: null });
+    }
+  }
+);
 
 export { router as currentUserRouter };
