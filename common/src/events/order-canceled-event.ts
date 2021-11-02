056e3a87c6b7e1f8bc0f0233999d5c6f518d932d
Denys Lins <denyslins@gmail.com>
2021-11-02 16:28:47 -0300

    Update event data for order creation and cancelation

--- common/src/events/order-canceled-event.ts
+++ common/src/events/order-canceled-event.ts
@@ -1,14 +1,11 @@
 import { Subjects } from "./subjects";
-import { OrderStatus } from "./types/order-status";
-import { TicketDoc } from "./types/ticket-doc";
 
 export interface OrderCanceledEvent {
   subject: Subjects.OrderCanceled;
   data: {
     id: string;
-    userId: string;
-    status: OrderStatus;
-    expiresAt: Date;
-    ticket: TicketDoc;
+    ticket: {
+      id: string;
+    };
   };
 }
--- common/src/events/order-created-event.ts
+++ common/src/events/order-created-event.ts
@@ -1,14 +1,16 @@
 import { OrderStatus } from "./types/order-status";
 import { Subjects } from "./subjects";
-import { TicketDoc } from "./types/ticket-doc";
 
 export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
     id: string;
-    userId: string;
     status: OrderStatus;
-    expiresAt: Date;
-    ticket: TicketDoc;
+    userId: string;
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    };
   };
 }
