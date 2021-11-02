a9101110656ffa699430c0dfc350d8359849a436
Denys Lins <denyslins@gmail.com>
2021-11-02 16:30:38 -0300

    Fix issues

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const isValidId = (id: string) => {
+export const isValidId = (id: string): boolean => {
   return mongoose.Types.ObjectId.isValid(id);
 };
 
